3276ca36c543379f385f34dd063f6d9b0c0e975d
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 16:36:12 -0400

    Added rotated polygon method, yet to test

--- src/cschacks/frontend/Polygons.java
+++ src/cschacks/frontend/Polygons.java
@@ -15,24 +15,25 @@ public class Polygons {
         for (int i = 0; i < sides; i++) {
             xValues[i] = x + (int) (rad * Math.cos(angle*i));
             yValues[i] = y + (int) (rad * Math.sin(angle*i));
-            // System.out.println("The x value for point " + i + " is " + xValues[i] + " and the y value is " + yValues[i]);
+            System.out.println("The x value for point " + i + " is " + xValues[i] + " and the y value is " + yValues[i]);
         }
         return new Polygon(xValues, yValues, sides);
     }
 
-    public static Polygon rotatePolygon(Polygon input, double angle) {
-    //     int sides = input.npoints;
-    //     int[] xValues = new int[sides];
-    //     int[] yValues = new int[sides];
-    //     double angleDiff = (2*Math.PI)/sides;
-    //     for (int i = 0; i < sides; i++) {
-    //         xValues[i] = input.xpoints[i] + (int) Math.cos((angleDiff*i) + angle);
-    //         yValues[i] = input.ypoints[i] + (int) Math.sin((angleDiff*i) + angle);
-    //         System.out.println("The x value for point " + i + " is " + xValues[i] + " and the y value is " + yValues[i]);
-    //     }
-        
-    //     return new Polygon(xValues, yValues, sides);
-    return new Polygon();
+    public static Polygon createRegularPolygon(int x, int y, int rad, int sides, int angle) {
+        if (sides < 3) {
+            System.out.println("Polygon unable to be drawn");
+            return new Polygon();
+        }
+        double angleDiff = (2*Math.PI)/sides;
+        int[] xValues = new int[sides];
+        int[] yValues = new int[sides];
+        for (int i = 0; i < sides; i++) {
+            xValues[i] = x + (int) (rad * Math.cos(angleDiff * i + angle));
+            yValues[i] = y + (int) (rad * Math.sin(angleDiff * i + angle));
+            System.out.println("The x value for point " + i + " is " + xValues[i] + " and the y value is " + yValues[i]);
+        }
+    return new Polygon(xValues, yValues, sides);
 
      }
         
