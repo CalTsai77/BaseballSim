4f7a0682b3b3f9e15e5b7d6a6ed2fa1ff19cd761
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 11:23:58 -0400

    Created a class called Polygons which will have methods for ease of creating polygons

---
+++ src/cschacks/frontend/Polygons.java
@@ -0,0 +1,18 @@
+package cschacks.frontend;
+
+import java.awt.Polygon;
+
+public class Polygons {
+
+    public static Polygon createRegularPolygon(int x, int y, int sides) {
+        return new Polygon();
+    }
+
+    public static Polygon rotatePolygon(Polygon input, double angle) {
+        return new Polygon();
+    }
+
+    public static Polygon translatePolygon(Polygon input, double x, double y) {
+        return new Polygon();
+    }
+}
