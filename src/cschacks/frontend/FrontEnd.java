0a998c154fd93ad91252bc56913eed7eb99ff44a
BuildTools <unconfigured@null.spigotmc.org>
2020-10-14 19:45:42 -0400

    Implemented curves

--- src/cschacks/frontend/FrontEnd.java
+++ src/cschacks/frontend/FrontEnd.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.Polygon;
 
 import javax.swing.JFrame;
@@ -15,7 +16,6 @@ import cschacks.frontend.Polygons;
 public class FrontEnd extends JPanel implements ActionListener {
 
     private JFrame fieldFrame;
-    private Graphics graphics;
     
     public FrontEnd () {
 		fieldFrame = new JFrame("Baseball Field");
@@ -27,13 +27,7 @@ public class FrontEnd extends JPanel implements ActionListener {
 
     public void showField () {
 		fieldFrame.setVisible(true);
-		graphics = getGraphics();
-		graphics.drawLine(505, 792, 620, 690);
-		graphics.drawLine(495, 792, 380, 690);
-		graphics.drawLine(620, 690, 860, 470);
-		graphics.drawLine(380, 690, 140, 470);
-		graphics.drawLine(140, 470, 140, 420);
-		graphics.drawLine(860, 470, 860, 420);
+		Graphics graphics = getGraphics();
 		Polygon homePlate = Polygons.createRegularPolygon(500, 800, 10, 5, (13*Math.PI)/10);
 		Polygon firstBase = Polygons.createRegularPolygon(620, 680, 10, 4);
 		Polygon secondBase = Polygons.createRegularPolygon(500, 560, 10, 4);
@@ -50,6 +44,28 @@ public class FrontEnd extends JPanel implements ActionListener {
 		graphics.drawPolygon(thirdBase);
 		graphics.setColor(Color.WHITE);
 		graphics.fillPolygon(thirdBase);
+		graphics.drawLine(505, 792, 620, 690);
+		graphics.drawLine(495, 792, 380, 690);
+		graphics.drawLine(620, 690, 860, 470);
+		graphics.drawLine(380, 690, 140, 470);
+		graphics.drawLine(140, 470, 140, 420);
+		graphics.drawLine(860, 470, 860, 420);
+		ArrayList<Line> ofFence = Polygons.createCurve(140, 470, 500, 270, 860, 470, 100);
+		ArrayList<Line> ifBorder = Polygons.createCurve(350, 660, 500, 540, 650, 660, 100);
+		ArrayList<Line> bottomIfBorder = Polygons.createCurve(/*find variables */);
+		graphics.setColor(Color.BLACK);
+		for (int i = 0; i < ofFence.size(); i++) {
+			Line l = ofFence.get(i);
+			graphics.drawLine(l.getX1(), l.getY1(), l.getX2(), l.getY2());
+		}
+		for (int i = 0; i < ifBorder.size(); i++) {
+			Line l = ifBorder.get(i);
+			graphics.drawLine(l.getX1(), l.getY1(), l.getX2(), l.getY2());
+		}
+		for(int i = 0; i < bottomIfBorder.size(); i++) {
+			Line l = bottomIfBorder.get(i);
+			graphics.drawLine(l.getX1(), l.getY1(), l.getX2(), l.getY2());
+		}
 	}
 	
 	public void hideField () {
--- src/cschacks/frontend/Polygons.java
+++ src/cschacks/frontend/Polygons.java
@@ -42,13 +42,15 @@ public class Polygons {
     
     public static ArrayList<Line> createCurve(int startX, int startY, int vertexX, int vertexY, int finishX, int finishY, int lines) {
     	ArrayList<Line> curve = new ArrayList<Line>();
-    	int a = (startY - vertexY)/(startX - vertexX);
-    	//y = a(x-vertexX) + vertexY
-    	int y1 = startY;
-    	for (int i = startX; i <= finishX; i+=((finishX-startX)/lines)) {
-    		int x2 = i+((finishX-startX)/lines);
-    		int y2 = a*(x2-vertexX) + vertexY;
-    		curve.add(new Line(i, y1, x2, y2));
+    	double a = ((double) startY - (double) vertexY)/Math.pow(((double) startX - (double) vertexX), 2);
+    	//y = a(x-vertexX)^2 + vertexY
+    	double y1 = startY;
+    	for (double i = startX; i < finishX; i+=((finishX-startX)/lines)) {
+            double x2 = i+((finishX-startX)/lines);
+    		double y2 = a*Math.pow((x2-vertexX), 2) + vertexY;
+            curve.add(new Line((int) i, (int) x2, (int) y1, (int) y2));
+            // System.out.println("Line goes from (" + i + ", " + y1 + ") to (" + x2 + ", " + y2 + ")");
+            y1 = y2;
     	}
     	return curve;
     }
